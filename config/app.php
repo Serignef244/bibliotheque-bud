c6e698ea2d428f4f20834469df722f2829903c0c
Serignef244 <serignef244@gmail.com>
2026-07-18 02:47:06 +0000

    fix: Hardcode APP_KEY in config to absolutely bypass Render environment variables

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:tSNczExZGU++h+UYtpyF0GyxCshQMrf5NydW7czl63Q=',
 
     'previous_keys' => [
         ...array_filter(
