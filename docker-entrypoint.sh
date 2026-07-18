fed4f69ac4f4a04f6bf3e3f337819722a666046a
Serignef244 <serignef244@gmail.com>
2026-07-18 02:31:47 +0000

    fix: Run migrations before optimize to avoid database cache missing table error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Exécution des migrations de la base de données..."
+php artisan migrate --force
+
 echo "Optimisation de l'application..."
 php artisan optimize:clear
 php artisan view:cache
 php artisan config:cache
 php artisan route:cache
-
-echo "Exécution des migrations de la base de données..."
-php artisan migrate --force
 
 echo "Démarrage du serveur Apache..."
 # Passe la main à la commande spécifiée dans le CMD du Dockerfile (apache2-foreground)
