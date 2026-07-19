e608fd12b5daa9931b5e1678839f433d8f5c2627
Serignef244 <serignef244@gmail.com>
2026-07-19 03:56:56 +0000

    chore: Remove DemoSeeder from main DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             UtilisateurSeeder::class,
-            DemoSeeder::class,
         ]);
     }
 }
